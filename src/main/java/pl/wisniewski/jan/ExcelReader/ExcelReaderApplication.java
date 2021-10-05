1782016961bd8d44aae36aae0310677d97049480
Janek <jan.krzysztof.wisniewski@gmail.com>
2021-10-05 22:32:12 +0200

    Initial commit

---
+++ src/main/java/pl/wisniewski/jan/ExcelReader/ExcelReaderApplication.java
@@ -0,0 +1,13 @@
+package pl.wisniewski.jan.ExcelReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelReaderApplication.class, args);
+	}
+
+}
